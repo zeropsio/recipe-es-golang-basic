df7d163e2cb72a469d3ccf54381d8c306542c90a
Miloš Lapiš <mlapis@mlc.cz>
2021-11-22 12:20:49 +0100

    wip(recipe): http request

--- app/server.go
+++ app/server.go
@@ -12,7 +12,7 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalf("... failed listening on port %s: %e, application not started.", port, err)
 	}
-	fmt.Printf("... listening on port %s, application started.", port)
+	fmt.Printf("... listening on port %s, application started.\n", port)
 }
 
 func ElasticSdk(w http.ResponseWriter, r *http.Request) {
