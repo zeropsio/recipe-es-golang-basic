c22b39255f378b03542c0ca4bddaaf6f6c476d72
Miloš Lapiš <mlapis@mlc.cz>
2021-11-22 12:26:20 +0100

    wip(recipe): http request

--- app/server.go
+++ app/server.go
@@ -10,12 +10,12 @@ func main() {
 	http.HandleFunc("/", ElasticSdk)
 	const port = "8080"
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("... failed listening on port %s: %e, application not started.", port, err)
+		log.Fatalf("... failed listening on port %s: %e, application not started.\n", port, err)
 	}
 	fmt.Printf("... listening on port %s, application started.\n", port)
 }
 
 func ElasticSdk(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "... Request: %s %s", r.Host, r.URL.Path)
-	fmt.Println(w, "... Hello! A new document was inserted into Elasticsearch!")
+	fmt.Fprintf(w, "... Request: %s %s", r.Host, r.URL.Path)
+	fmt.Fprintf(w, "... Hello! A new document was inserted into Elasticsearch!")
 }
